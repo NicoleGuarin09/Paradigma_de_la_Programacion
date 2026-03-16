666578b89b9c0112857223a4fe5472f27fee0099
NicoleGuarin09 <nicolguarin23@gmal.com>
2026-03-16 16:19:25 -0500

    Mi primer servidor

---
+++ Backend/Mi_primer_servidor.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('¡Holaa, mundo!');
+});
+
+app.get('/json', (req, res ) => {
+    res.json({mensaje: '¡Holaa, mundo en formato JSON!'});
+});
+
+app.listen(3000, () => {
+    console.log('Servidor escuchando en el puerto 3000');
+});
+
+
+app.get('/json', (req, res) => {
+    res.json({mensaje: 'Hola Mundo PCJIC'});
+});
+
+
+
+app.get('/info', (req, res) => {
+    res.json({Nombre: 'Nicole', Apellido: 'Guarin', Edad: '16', colegio: 'La Salle de Campoamo'});
+    
+});
